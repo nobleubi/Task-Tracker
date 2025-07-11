51ca47deb11fc509a44c73b623803d02f05fe7a3
nobleubi <117305311+nobleubi@users.noreply.github.com>
2025-07-11 04:20:24 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
 import About from "./components/About";
 
-const API_URL = 'https://your-app-name.onrender.com/tasks'; // Replace with your actual Render URL
+const API_URL = https://task-tracker-backend-6bzc.onrender.com'; // Replace with your actual Render URL
 
 const App = () => {
   const [showAddTask, setShowAddTask] = useState(false);
