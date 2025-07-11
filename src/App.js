4069014e063d2d3f090dacb6bc4b9ffb917e4e5e
nobleubi <117305311+nobleubi@users.noreply.github.com>
2025-07-11 05:05:28 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
 import About from "./components/About";
 
-const API_URL = "https://task-tracker-backend-6bzc.onrender.com";
+const API_URL = "https://my-json-server.typicode.com/nobleubi/Task-Tracker/tasks";
 
 const App = () => {
   const [showAddTask, setShowAddTask] = useState(false);
