00d9edc7cc86eaa7f8d7c131392f290d626cfdce
nobleubi <117305311+nobleubi@users.noreply.github.com>
2025-07-11 03:35:57 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
 import About from "./components/About";
 
+const API_URL = "https://my-json-server.typicode.com/nobleubi/Task-Tracker";
+
 const App = () => {
   const [showAddTask, setShowAddTask] = useState(false);
   const [tasks, setTasks] = useState([]);
@@ -15,87 +17,85 @@ const App = () => {
       const tasksFromServer = await fetchTasks();
       setTasks(tasksFromServer);
     };
-
     getTasks();
   }, []);
 
   // Fetch all tasks
   const fetchTasks = async () => {
-    const res = await fetch('http://localhost:5000/tasks');
-    const data = await res.json();
-    return data;
+    const res = await fetch(`${API_URL}/tasks`);
+    return await res.json();
   };
 
-  // Fetch a single task
+  // Fetch single task
   const fetchTask = async (id) => {
-    const res = await fetch(`http://localhost:5000/tasks/${id}`);
-    const data = await res.json();
-    return data;
+    const res = await fetch(`${API_URL}/tasks/${id}`);
+    return await res.json();
   };
 
-  // Add task
+  // Add task (this will not save permanently on my-json-server)
   const addTask = async (task) => {
-    const res = await fetch('http://localhost:5000/tasks', {
+    const res = await fetch(`${API_URL}/tasks`, {
       method: 'POST',
       headers: {
-        'Content-type': 'application/json'
+        'Content-type': 'application/json',
       },
-      body: JSON.stringify(task)
+      body: JSON.stringify(task),
     });
 
     const data = await res.json();
-    setTasks([...tasks, data]);
+    setTasks([...tasks, data]); // temporary in browser
   };
 
   // Delete task
   const deleteTask = async (id) => {
-    await fetch(`http://localhost:5000/tasks/${id}`, {
-      method: 'DELETE'
+    await fetch(`${API_URL}/tasks/${id}`, {
+      method: 'DELETE',
     });
+
     setTasks(tasks.filter((task) => task.id !== id));
   };
 
   // Toggle reminder
   const toggleReminder = async (id) => {
     const taskToToggle = await fetchTask(id);
-    const updatedTask = { ...taskToToggle, reminder: !taskToToggle.reminder };
+    const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder };
 
-    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+    const res = await fetch(`${API_URL}/tasks/${id}`, {
       method: 'PUT',
       headers: {
-        'Content-type': 'application/json'
+        'Content-type': 'application/json',
       },
-      body: JSON.stringify(updatedTask)
+      body: JSON.stringify(updTask),
     });
 
     const data = await res.json();
-    setTasks(tasks.map((task) =>
-      task.id === id ? { ...task, reminder: data.reminder } : task
-    ));
+
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: data.reminder } : task
+      )
+    );
   };
 
   return (
     <Router>
-      <div className='container'>
-        <Header
-          onAdd={() => setShowAddTask(!showAddTask)}
-          showAdd={showAddTask}
-        />
+      <div className="container">
+        <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
         <Routes>
           <Route
-            path='/'
+            path="/"
             element={
               <>
                 {showAddTask && <AddTask onAdd={addTask} />}
                 {tasks.length > 0 ? (
                   <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
                 ) : (
-                  'No Tasks To Show'
+                  "No Tasks To Show"
                 )}
               </>
             }
           />
-          <Route path='/about' element={<About />} />
+          <Route path="/about" element={<About />} />
         </Routes>
         <Footer />
       </div>
